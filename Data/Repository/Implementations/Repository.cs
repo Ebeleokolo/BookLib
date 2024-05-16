cf6df55890ead13e5b8e25ac8e7902a5be9ca1b8
Ebeleokolo <ebele.okolo@decagon.dev>
2024-05-16 02:25:19 +0100

    Depedency injected

---
+++ Data/Repository/Implementations/Repository.cs
@@ -0,0 +1,38 @@
+﻿using BookLib.Data.Repository.Interfaces;
+
+namespace BookLib.Data.Repository.Implementations
+{
+    public class Repository : IRepository
+    {
+        private readonly BookLibDbContext _ctx;
+        public Repository(BookLibDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public Task<int> Add<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Delete<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get<T>(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookLib.Models.Entities;
+
+namespace BookLib.Data.Repository.Interfaces 
+{
+    public interface IRepository
+    {
+        Task<int> Add<T>(T entity);
+        Task<int> Update<T>(T entity);
+        Task<int> Delete<T>(T entity);
+        Task<IQueryable<T>> Get<T>();
+        Task<T> Get<T>(string Id);
+    }
+}
